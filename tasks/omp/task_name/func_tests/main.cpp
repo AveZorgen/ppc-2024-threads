dcd64d26476383035120b7471427bde82b4ea1c1
AveZorgen <78862872+AveZorgen@users.noreply.github.com>
2024-04-10 22:09:18 +0300

    11

--- tasks/omp/task_name/func_tests/main.cpp
+++ tasks/omp/task_name/func_tests/main.cpp
@@ -124,7 +124,7 @@ TEST(task_name_omp, Test_Sum_70) {
 }
 
 TEST(task_name_omp, Test_Sum_100) {
-  int n = 1000, h = 600;
+  int n = 500, h = 300;
   std::vector<jarvis::r> points(n);
   std::vector<jarvis::r> hull(h);
   std::vector<jarvis::r> out(hull.size());
